5450b45fd8faa5f49a3d142a5196838f5a92d4a0
huangduyu <huangduyu@douban.com>
2014-07-16 10:40:39 +0800

    geekerDetail: delete initWithNibName method

--- HDYSoccer/HDYSoccerGeekerDetailViewController.m
+++ HDYSoccer/HDYSoccerGeekerDetailViewController.m
@@ -16,15 +16,6 @@
 
 @implementation HDYSoccerGeekerDetailViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-  if (self) {
-    // Custom initialization
-  }
-  return self;
-}
-
 - (id)initWithGeeker:(NSString *)geekerName
 {
   self = [super initWithStyle:UITableViewStyleGrouped];
