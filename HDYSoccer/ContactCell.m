c0f2992fd5248bcb3ec1458d5b35a225e4435ebe
huangduyu <huangduyu@douban.com>
2014-07-28 18:28:11 +0800

    create_game: config contact cell

--- HDYSoccer/ContactCell.m
+++ HDYSoccer/ContactCell.m
@@ -7,6 +7,13 @@
 //
 
 #import "ContactCell.h"
+#import "GameListFilterTableViewCell.h"
+
+// image
+#define CONTACT_IMAGE_LEFT_MARGIN 10.0F
+#define CONTACT_IMAGE_TOP_MARGIN 14.0F
+#define CONTACT_IMAGE_WIDTH 13.0F
+#define CONTACT_IMAGE_HEIGHT 13.0F
 
 // title
 #define TITLE_LEFT_MARGIN 10.0f
@@ -24,16 +31,34 @@
 {
   self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
   if (self) {
+    [self setBackgroundColor:[UIColor clearColor]];
+    [self.contentView setBackgroundColor:[UIConfiguration colorForHex:GAME_LIST_FILTER_CELL_BACKGROUND_COLOR]];
+    [self.contentView setAlpha:0.5];
+    
+    // contact image
+    UIImage *contactImage = [UIImage imageNamed:@"phone2-75.png"];
+    CGRect contactRect = CGRectMake(CONTACT_IMAGE_LEFT_MARGIN, CONTACT_IMAGE_TOP_MARGIN, CONTACT_IMAGE_WIDTH, CONTACT_IMAGE_HEIGHT);
+    UIImageView *contactView = [[UIImageView alloc] initWithFrame:contactRect];
+    [contactView setImage:contactImage];
+    
+    [self addSubview:contactView];
+    
     // title
-    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(TITLE_LEFT_MARGIN, 0, 0, 0)];
+    CGFloat titleX = CGRectGetMaxX(contactView.frame) + TITLE_LEFT_MARGIN;
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(titleX, 0, 0, 0)];
     [titleLabel setText:title];
     [titleLabel sizeToFit];
+    [titleLabel setTextColor:[UIColor whiteColor]];
     [UIConfiguration moveSubviewYToSuperviewCenter:self.contentView subview:titleLabel];
+    
+    [self addSubview:titleLabel];
     
     // text field
     CGFloat fieldX = CGRectGetMaxX(titleLabel.frame) + TEXT_FIELD_LEFT_MARGIN;
     CGRect fieldRect = CGRectMake(fieldX, 0, TEXT_FIELD_WIDTH, TEXT_FIELD_HEIGHT);
     UITextField *textField = [[UITextField alloc] initWithFrame:fieldRect];
+    [textField setBackgroundColor:[UIColor whiteColor]];
+    [textField setTintColor:[UIColor blackColor]];
     [textField.layer setBorderWidth:1.0f];
     [textField.layer setCornerRadius:5.0f];
     [textField.layer setBorderColor:[UIColor lightGrayColor].CGColor];
@@ -41,9 +66,7 @@
     [UIConfiguration moveSubviewYToSuperviewCenter:self.contentView subview:textField];
     
     self.textField = textField;
-    
-    [self.contentView addSubview:titleLabel];
-    [self.contentView addSubview:textField];
+    [self addSubview:textField];
     
     [self setSelectionStyle:UITableViewCellSelectionStyleNone];
   }
