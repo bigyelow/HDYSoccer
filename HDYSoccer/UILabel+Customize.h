aabf5360e1e156d77285db2d75f0ceba3e8df5c6
huangduyu <huangduyu@douban.com>
2014-07-31 18:19:41 +0800

    UI: config uilabel

---
+++ HDYSoccer/UILabel+Customize.h
@@ -0,0 +1,21 @@
+//
+//  UILabel+Customize.h
+//  HDYSoccer
+//
+//  Created by bigyelow on 7/31/14.
+//  Copyright (c) 2014 bigyelow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (Customize)
+
+- (void)configWithText:(NSString *)text
+             textColor:(UIColor *)textColor
+                  font:(UIFont *)font
+         numberOfLines:(NSInteger)numberOfLines;
+
+- (void)configWithText:(NSString *)text
+             textColor:(UIColor *)textColor
+                  font:(UIFont *)font;
+@end
---
+++ HDYSoccer/UILabel+Customize.m
@@ -0,0 +1,31 @@
+//
+//  UILabel+Customize.m
+//  HDYSoccer
+//
+//  Created by bigyelow on 7/31/14.
+//  Copyright (c) 2014 bigyelow. All rights reserved.
+//
+
+#import "UILabel+Customize.h"
+
+@implementation UILabel (Customize)
+
+- (void)configWithText:(NSString *)text
+             textColor:(UIColor *)textColor
+                  font:(UIFont *)font
+         numberOfLines:(NSInteger)numberOfLines
+{
+  [self setText: text];
+  [self setTextColor:textColor];
+  [self setFont:font];
+  [self setNumberOfLines:numberOfLines];
+  [self sizeToFit];
+}
+
+- (void)configWithText:(NSString *)text
+             textColor:(UIColor *)textColor
+                  font:(UIFont *)font
+{
+  [self configWithText:text textColor:textColor font:font numberOfLines:0];
+}
+@end
