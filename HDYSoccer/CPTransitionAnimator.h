2086cd942c075483266a6c5810ce96d2ca64e1de
huangduyu <huangduyu@douban.com>
2014-08-29 16:14:16 +0800

    UI: add CPAnimatior

---
+++ HDYSoccer/CPTransitionAnimator.h
@@ -0,0 +1,21 @@
+//
+//  CPTransitionAnimator.h
+//  HDYSoccer
+//
+//  Created by bigyelow on 8/29/14.
+//  Copyright (c) 2014 bigyelow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define CPTRANSITION_ENDFRAME_WIDTH (WINDOW_FRAME.size.width / 5 * 3)
+#define CPTRANSITION_ENDFRAME_HEIGHT (WINDOW_FRAME.size.height)
+#define CPTRANSITION_ENDFRAME_X (WINDOW_FRAME.size.width / 5 * 2)
+#define CPTRANSITION_ENDFRAME_Y (0)
+#define CPTRANSITION_ENDFRAME \
+        CGRectMake(CPTRANSITION_ENDFRAME_X, CPTRANSITION_ENDFRAME_Y, CPTRANSITION_ENDFRAME_WIDTH, CPTRANSITION_ENDFRAME_HEIGHT)
+
+@interface CPTransitionAnimator : NSObject <UIViewControllerAnimatedTransitioning>
+
+@property (nonatomic, assign) BOOL presenting;
+@end
---
+++ HDYSoccer/CPTransitionAnimator.m
@@ -0,0 +1,60 @@
+//
+//  CPTransitionAnimator.m
+//  HDYSoccer
+//
+//  Created by bigyelow on 8/29/14.
+//  Copyright (c) 2014 bigyelow. All rights reserved.
+//
+
+#import "CPTransitionAnimator.h"
+
+@implementation CPTransitionAnimator
+
+- (NSTimeInterval)transitionDuration:(id <UIViewControllerContextTransitioning>)transitionContext
+{
+  return 0.5f;
+}
+
+- (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext
+{
+  UIViewController *fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+  UIViewController *toViewController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+  
+  CGRect endFrame = CPTRANSITION_ENDFRAME;
+  
+  if (self.presenting) {
+    fromViewController.view.userInteractionEnabled = NO;
+    
+    [transitionContext.containerView addSubview:fromViewController.view];
+    [transitionContext.containerView addSubview:toViewController.view];
+    
+    CGRect startFrame = endFrame;
+    startFrame.origin.x += (WINDOW_FRAME.size.width - CPTRANSITION_ENDFRAME_X);
+    
+    toViewController.view.frame = startFrame;
+    
+    [UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
+      fromViewController.view.tintAdjustmentMode = UIViewTintAdjustmentModeDimmed;
+      toViewController.view.frame = endFrame;
+    } completion:^(BOOL finished) {
+      [transitionContext completeTransition:YES];
+    }];
+  }
+  else {
+    toViewController.view.userInteractionEnabled = YES;
+    
+    [transitionContext.containerView addSubview:toViewController.view];
+    [transitionContext.containerView addSubview:fromViewController.view];
+    
+    endFrame.origin.x += (WINDOW_FRAME.size.width - CPTRANSITION_ENDFRAME_X);
+    
+    [UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
+      toViewController.view.tintAdjustmentMode = UIViewTintAdjustmentModeAutomatic;
+      fromViewController.view.frame = endFrame;
+    } completion:^(BOOL finished) {
+      [transitionContext completeTransition:YES];
+    }];
+  }
+}
+
+@end
