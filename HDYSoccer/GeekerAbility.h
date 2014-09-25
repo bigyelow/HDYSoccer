0cbd08bd95c64a5856c638ef2980d9c099dd948c
huangduyu <huangduyu@douban.com>
2014-09-25 17:38:28 +0800

    model: add PlayerAbility, abilityStringFromAbility, abilityTypeFromAbilityName method

--- HDYSoccer/GeekerAbility.h
+++ HDYSoccer/GeekerAbility.h
@@ -11,10 +11,30 @@
 #import "SkillAbility.h"
 #import "QualityAbility.h"
 
+typedef NS_ENUM(NSUInteger, PlayerAbility) {
+  kPlayerAbilityAttack,
+  kPlayerAbilityDefend,
+  kPlayerAbilityPass,
+  kPlayerAbilityShoot,
+  kPlayerAbilityBalllessRunning,
+  kPlayerAbilityPlaceKick,
+  kPlayerAbilityHeading,
+  kPlayerAbilityRespond,
+  kPlayerAbilityTeamSpirit,
+  kPlayerAbilityDevotion,
+  kPlayerAbilityCreativity,
+  kPlayerAbilityStamina,
+  kPlayerAbilitySpeed,
+  kPlayerAbilityStrong,
+  kPlayerAbilityAggressivity
+};
+
 @interface GeekerAbility : BaseObject
 
 @property (nonatomic, strong) ComprehensiveAbility *comprehensiveAbility;
 @property (nonatomic, strong) SkillAbility *skillAbility;
 @property (nonatomic, strong) QualityAbility *qualityAbility;
 
++ (NSString *)abilityStringFromAbility:(PlayerAbility)ability;
++ (PlayerAbility)abilityTypeFromAbilityName:(NSString *)name;
 @end
--- HDYSoccer/GeekerAbility.m
+++ HDYSoccer/GeekerAbility.m
@@ -34,4 +34,108 @@
   }
   return _qualityAbility;
 }
+
++ (PlayerAbility)abilityTypeFromAbilityName:(NSString *)name
+{
+  if ([name isEqualToString:TEXT_ABILITY_TEAM_SPIRITY]) {
+    return kPlayerAbilityTeamSpirit;
+  }
+  else if ([name isEqualToString:TEXT_ABILITY_STRONG]) {
+    return kPlayerAbilityStrong;
+  }
+  else if ([name isEqualToString:TEXT_ABILITY_STAMINA]) {
+    return kPlayerAbilityStamina;
+  }
+  else if ([name isEqualToString:TEXT_ABILITY_SPEED]) {
+    return kPlayerAbilitySpeed;
+  }
+  else if ([name isEqualToString:TEXT_ABILITY_SHOOT]) {
+    return kPlayerAbilityShoot;
+  }
+  else if ([name isEqualToString:TEXT_ABILITY_RESPOND]) {
+    return kPlayerAbilityRespond;
+  }
+  else if ([name isEqualToString:TEXT_ABILITY_PLACE_KICK]) {
+    return kPlayerAbilityPlaceKick;
+  }
+  else if ([name isEqualToString:TEXT_ABILITY_PASS]) {
+    return kPlayerAbilityPass;
+  }
+  else if ([name isEqualToString:TEXT_ABILITY_HEADING]) {
+    return kPlayerAbilityHeading;
+  }
+  else if ([name isEqualToString:TEXT_ABILITY_DEVOTION]) {
+    return kPlayerAbilityDevotion;
+  }
+  else if ([name isEqualToString:TEXT_ABILITY_DEFEND]) {
+    return kPlayerAbilityDefend;
+  }
+  else if ([name isEqualToString:TEXT_ABILITY_CREATIVITY]) {
+    return kPlayerAbilityCreativity;
+  }
+  else if ([name isEqualToString:TEXT_ABILITY_BALLLESS_RUNNING]) {
+    return kPlayerAbilityBalllessRunning;
+  }
+  else if ([name isEqualToString:TEXT_ABILITY_ATTACK]) {
+    return kPlayerAbilityAttack;
+  }
+  else if ([name isEqualToString:TEXT_ABILITY_AGGRESSIVITY]) {
+    return kPlayerAbilityAggressivity;
+  }
+  
+  return 0;
+}
+
++ (NSString *)abilityStringFromAbility:(PlayerAbility)ability
+{
+  switch (ability) {
+    case kPlayerAbilityAggressivity:
+      return @"aggressivity";
+      
+    case kPlayerAbilityAttack:
+      return @"attack";
+      
+    case kPlayerAbilityBalllessRunning:
+      return @"ballless_running";
+      
+    case kPlayerAbilityCreativity:
+      return @"creativity";
+      
+    case kPlayerAbilityDefend:
+      return @"defend";
+      
+    case kPlayerAbilityDevotion:
+      return @"devotion";
+      
+    case kPlayerAbilityHeading:
+      return @"heading";
+      
+    case kPlayerAbilityPass:
+      return @"pass";
+      
+    case kPlayerAbilityPlaceKick:
+      return @"place_kick";
+      
+    case kPlayerAbilityRespond:
+      return @"respond";
+      
+    case kPlayerAbilityShoot:
+      return @"shoot";
+      
+    case kPlayerAbilitySpeed:
+      return @"speed";
+      
+    case kPlayerAbilityStamina:
+      return @"stamina";
+      
+    case kPlayerAbilityStrong:
+      return @"strong";
+      
+    case kPlayerAbilityTeamSpirit:
+      return @"team_spirit";
+      
+    default:
+      break;
+  }
+}
 @end
